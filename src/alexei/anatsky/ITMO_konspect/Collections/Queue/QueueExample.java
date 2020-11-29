7da962f32c3ee1e524127214ebed6a220c70099f
naglec13 <naglec13@gmail.com>
2020-11-29 23:45:46 +0300

    queue, priorityQueue

---
+++ src/alexei/anatsky/ITMO_konspect/Collections/Queue/QueueExample.java
@@ -0,0 +1,59 @@
+package alexei.anatsky.ITMO_konspect.Collections.Queue;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if ((o1 % 2 == 0 && o2 % 2 == 0) || (o1 % 2 != 0 && o2 % 2 != 0)) {
+                    if (o1 > o2) {
+                        return 1;
+                    } else if (o1 < o2) {
+                        return -1;
+                    } else return 0;
+                } else if (o1 % 2 == 0) {
+                        return 1;
+                    }else {
+                        return -1;
+                    }
+            }
+        });
+
+        fillQueue(queue, 5);
+        fillQueue(priorityQueue, 5);
+
+        printQueue(queue);
+        printEmptyQueue(queue, "Queue");
+
+        printQueue(priorityQueue);
+        printEmptyQueue(priorityQueue, "PriorityQueue");
+
+    }
+
+    private static void printQueue(Queue<Integer> queue) {
+        System.out.println("*** " + queue.getClass().getSimpleName() + " ***");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
+
+    private static void printEmptyQueue(Queue<Integer> queue, String name) {
+        if (queue.isEmpty()) {
+            System.out.println(name + " is empty!");
+            System.out.println("_________________________________");
+        }
+    }
+
+    private static void fillQueue(Queue<Integer> e, int bound) {
+        while (bound > 0) {
+            e.add(bound);
+            bound--;
+        }
+    }
+}
