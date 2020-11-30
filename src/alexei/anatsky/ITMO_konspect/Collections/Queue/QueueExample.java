d1a7a78f6a9e9e9118043c1e3ac41756a20fa1ba
naglec13 <naglec13@gmail.com>
2020-11-30 23:57:36 +0300

    compareTo

--- src/alexei/anatsky/ITMO_konspect/Collections/Queue/QueueExample.java
+++ src/alexei/anatsky/ITMO_konspect/Collections/Queue/QueueExample.java
@@ -12,11 +12,7 @@ public class QueueExample {
             @Override
             public int compare(Integer o1, Integer o2) {
                 if ((o1 % 2 == 0 && o2 % 2 == 0) || (o1 % 2 != 0 && o2 % 2 != 0)) {
-                    if (o1 > o2) {
-                        return 1;
-                    } else if (o1 < o2) {
-                        return -1;
-                    } else return 0;
+                    return o1.compareTo(o2);
                 } else if (o1 % 2 == 0) {
                         return 1;
                     }else {
